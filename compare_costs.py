82b7954c7e6eef651e50288f534bab0b53dc4207
addyj4922 <31721415+addyj4922@users.noreply.github.com>
2018-12-25 09:28:57 +0530

    Update compare_costs.py

--- compare_costs.py
+++ compare_costs.py
@@ -1,9 +1,7 @@
-
-
-eval(s)=input("enter weight and price").split(" ")
-eval(s1)=input("enter weight and price").split(" ")
-print( s,s1)
-if s[0]/s[1]<s1[0]/s1[1]:
+s = input("enter Price1 and Weight1").split(" ")
+s1 = input("enter Price2 and Weight2").split(" ")
+print(s, s1)
+if (float(s[0])/float(s[1])) < (float(s1[0])/float(s1[1])):
     print("pack 1")
 else:
     print("pack 2")
