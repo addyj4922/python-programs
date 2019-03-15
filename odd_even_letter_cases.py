789a7b6ebb69bf563643d6fcf92d6d1a3d4c5ffd
addyj4922 <31721415+addyj4922@users.noreply.github.com>
2019-03-15 10:26:09 +0530

    Update odd_even_letter_cases.py

--- odd_even_letter_cases.py
+++ odd_even_letter_cases.py
@@ -2,13 +2,14 @@ def myfunc(name):
     a=name.lower()
     l=list(a)
     l1=[]
-    print(l)
+    s=''
     for i in range(0,len(l)):
         if i%2==0:
             b=l[i].upper()
             l1.append(b)
         else:
             l1.append(l[i])
-    print(l1)
+    s=''.join(l1)
+    print(s)
 
 myfunc('heLLO worLd')
