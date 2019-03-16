abd4b75738e6fdb07e822025b53926e58389b30e
addyj4922 <31721415+addyj4922@users.noreply.github.com>
2019-03-16 17:12:02 +0530

    Update animal_cracker.py

--- animal_cracker.py
+++ animal_cracker.py
@@ -1,5 +1,4 @@
 def animal_crack(a):
-    b=a.lower()
-    wordlist=b.split()
+    wordlist=a.lower().split()
     return wordlist[0][0]==wordlist[1][0]
 print(animal_crack(str(input("Enter a 2 word string"))
