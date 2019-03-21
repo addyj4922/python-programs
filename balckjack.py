a99cf282bf04ea5d9cf81b18599b0bdbc40a1509
addyj4922 <31721415+addyj4922@users.noreply.github.com>
2019-03-21 15:07:24 +0530

    Delete balckjack.py

--- balckjack.py
+++
@@ -1,9 +0,0 @@
-def blackjack(a,b,c):
-    s=a+b+c
-    if s<=21:
-        return s
-    elif s<=31 and (a==11 or b==11 or c==11):
-        return (s-10)
-    else:
-        return 'BUST'
-print(blackjack(9,9,9))
